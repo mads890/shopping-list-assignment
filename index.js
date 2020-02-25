da376dbc9ac7724b72124c046d971f80cfc62802
mads890 <59069830+mads890@users.noreply.github.com>
2020-02-24 19:44:37 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,18 @@
-function isChecked() {
-    $('shopping-item-toggle').on('click', function(event) {
-        let targetItem = $(this.find('.shopping-item'));
+$(function() {
+
+    $('ul').on('click', '.shopping-item-toggle', function(event) {
+        let targetItem = $(this).closest('.shopping-item');
         let pressedKB = this.attr('aria-pressed') === 'true';
-        targetItem.toggleClass('.shopping-item__checked').attr('aria-pressed', !pressedKB);
+        targetItem.toggleClass('shopping-item__checked').attr('aria-pressed', !pressedKB);
     });
-};
 
-$(isChecked);
-
-function deleteItem() {
     $('ul').on('click', '.shopping-item-delete', function(event) {
-        this.remove('li');
+        $(this).closest('li').remove();
     });
-};
 
-$(deleteItem);
-
-function addItem() {
-    $('#js-shopping-list-form').submit(function(event) {
+    $('#js-shopping-list-form').on('submit click', function(event) {
         event.preventDefault();
-        const listItem = $(this.find('#shopping-list-entry');
-        $('.shopping-list').append('<li> <span class="shopping-item">' + listItem + '</span> <div class="shopping-item-controls">'
-        + '<button class="shopping-item-toggle"> <span class="button-label">check</span> </button>' +
-        '<button class="shopping-item-delete"> <span class="button-label">delete</span> </button> </div> </li>');
+        let listItem = $(this).find('#shopping-list-entry').val();
+        $('.shopping-list').append(`<li><span class="shopping-item">${listItem}</span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button><button class="shopping-item-delete"><span class="button-label">delete</span></button></div></li>`)
     });
-};
-
-$(addItem);
+});
