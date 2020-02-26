5142466665999ad389824a9542a39e9aacc98356
mads890 <59069830+mads890@users.noreply.github.com>
2020-02-26 09:19:17 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ $(function() {
 
     $('.shopping-list').on('click', '.shopping-item-toggle', function(event) {
         let pressedKB = this.attr('aria-pressed') === 'true';
-        $(this).parent('.shopping-item-controls').sibling('.shopping-item').toggleClass('shopping-item__checked').attr('aria-pressed', !pressedKB);
+        $(this).closest('.shopping-item-controls').sibling('.shopping-item').toggleClass('shopping-item__checked').attr('aria-pressed', !pressedKB);
     });
 
     $('ul').on('click', '.shopping-item-delete', function(event) {
@@ -13,6 +13,6 @@ $(function() {
         event.preventDefault();
         let listItem = $(this).find('#shopping-list-entry').val();
         $('.shopping-list').append(`<li><span class="shopping-item">${listItem}</span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button><button class="shopping-item-delete"><span class="button-label">delete</span></button></div></li>`)
-        $(this).reset();
+        $(this).val('');
     });
 });
