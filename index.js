dc987be00f5e32417f5e2f98aceb0b48919bd1f8
mads890 <59069830+mads890@users.noreply.github.com>
2020-02-26 15:50:41 -0500

    Update index.js

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ $(function() {
     $('#js-shopping-list-form').on('submit', function(event) {
         event.preventDefault();
         let listItem = $(this).find('#shopping-list-entry').val();
-        $('.shopping-list').append(`<li><span class="shopping-item">${listItem}</span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button><button class="shopping-item-delete"><span class="button-label">delete</span></button></div></li>`)
+        $('.shopping-list').append(`<li><span class="shopping-item">${listItem}</span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button><button class="shopping-item-delete"><span class="button-label">delete</span></button></div></li>`);
+        $('#shopping-list-entry').val('')
     });
 });
