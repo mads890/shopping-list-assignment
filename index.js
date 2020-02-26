14e286f8a5bdf1e6a342a23d652c5cedb5698c1f
mads890 <59069830+mads890@users.noreply.github.com>
2020-02-26 14:57:29 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
   $(function() {
 
     $('.shopping-list').on('click', '.shopping-item-toggle', function(event) {
+      console.log(event);
         let pressedKB = this.attr('aria-pressed') === 'true';
         let targetItem = $(this).closest('.shopping-item-controls').sibling('.shopping-item');
         targetItem.toggleClass('shopping-item__checked').attr('aria-pressed', !pressedKB);
@@ -14,6 +15,5 @@
         event.preventDefault();
         let listItem = $(this).find('#shopping-list-entry').val();
         $('.shopping-list').append(`<li><span class="shopping-item">${listItem}</span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button><button class="shopping-item-delete"><span class="button-label">delete</span></button></div></li>`)
-        $(this).val('');
     });
 });
